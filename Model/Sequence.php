1f37a36d838a8a4bcf75b87797835525175eae06
Timon de Groot <timon@marissen.net>
2019-06-28 14:31:59 +0200

    Initial commit

---
+++ Model/Sequence.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marissen\BetterOrderIncrementing\Model;
+
+use Magento\Framework\App\ResourceConnection as AppResource;
+use Magento\Framework\DB\Sequence\SequenceInterface;
+use Magento\SalesSequence\Model\Meta;
+use Magento\SalesSequence\Model\Profile;
+
+class Sequence implements SequenceInterface
+{
+    /**
+     * Default pattern for Sequence
+     */
+    const DEFAULT_PATTERN  = "%s%'.09d%s";
+    /**
+     * @var string
+     */
+    private $lastIncrementId;
+    /**
+     * @var Meta
+     */
+    private $meta;
+    /**
+     * @var false|\Magento\Framework\DB\Adapter\AdapterInterface
+     */
+    private $connection;
+    /**
+     * @var string
+     */
+    private $pattern;
+
+    /**
+     * @param Meta $meta
+     * @param AppResource $resource
+     * @param string $pattern
+     */
+    public function __construct(
+        Meta $meta,
+        AppResource $resource,
+        $pattern = self::DEFAULT_PATTERN
+    ) {
+        $this->meta = $meta;
+        $this->connection = $resource->getConnection('sales');
+        $this->pattern = $pattern;
+    }
+
+    /**
+     * Retrieve current sequence value.
+     *
+     * @return string
+     */
+    public function getCurrentValue()
+    {
+        if (!isset($this->lastIncrementId)) {
+            return null;
+        }
+
+        return sprintf(
+            $this->pattern,
+            $this->getMetaActiveProfile()->getData('prefix'),
+            $this->calculateCurrentValue(),
+            $this->getMetaActiveProfile()->getData('suffix')
+        );
+    }
+
+    /**
+     * Retrieve next sequence value.
+     *
+     * @return string
+     * @throws \Zend_Db_Statement_Exception
+     */
+    public function getNextValue()
+    {
+        $this->connection->insert(
+            $this->getMetaSequenceTable(),
+            [
+                'sequence_value' => $this->getLatestId() + 1
+            ]
+        );
+
+        $this->lastIncrementId = $this->getLatestId();
+
+        return $this->getCurrentValue();
+    }
+
+    /**
+     * Returns the latest id from the sequence table.
+     *
+     * @return int
+     * @throws \Zend_Db_Statement_Exception
+     */
+    private function getLatestId(): int
+    {
+        $select = $this->connection->select()
+            ->from($this->getMetaSequenceTable())
+            ->columns('sequence_value')
+            ->order('sequence_value ' . \Magento\Framework\DB\Select::SQL_DESC)
+            ->limit(1);
+
+        $lastRow = $this->connection->query($select)->fetch();
+
+        return is_array($lastRow) ? (int) $lastRow['sequence_value'] : 0;
+    }
+
+    /**
+     * Returns current value based on step and start value.
+     *
+     * @return string
+     */
+    private function calculateCurrentValue()
+    {
+        return ($this->lastIncrementId - $this->getMetaActiveProfile()->getData('start_value'))
+            * $this->getMetaActiveProfile()->getData('step') + $this->getMetaActiveProfile()->getData('start_value');
+    }
+
+    /**
+     * Returns the active profile from the sequence meta.
+     *
+     * @return \Magento\SalesSequence\Model\Profile
+     */
+    private function getMetaActiveProfile(): Profile
+    {
+        return $this->meta->getData('active_profile');
+    }
+
+    /**
+     * Returns the sequence table from the sequence meta.
+     *
+     * @return string
+     */
+    private function getMetaSequenceTable(): string
+    {
+        return $this->meta->getData('sequence_table');
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Magento 2 Better Order Incrementing
+
+Increment order ids manually, instead of relying on auto increment.
+
+When MySQL rollbacks happen, auto increment IDs do not rollback to their previous value. In many cases this isn't a bad thing, but it is for Order IDs. Order IDs need to be sequential for better organization and bookkeeping. 
+
+## Installation
+``` bash
+composer require marissen/magento2-better-order-incrementing
+bin/magento setup:upgrade
+```
+
+## Uninstallation
+The auto_increment for the following tables probably are not in sync with the content of said tables:
+- `sequence_creditmemo_X` 
+- `sequence_invoice_X` 
+- `sequence_order_X` 
+- `sequence_shipment_X` 
+
+Please verify this and make sure it is in sync, because if they are not and you disable/uninstall this module, you'll run into MySQL constraints.
